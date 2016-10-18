a4d5fc89d7ef991ff60fa7149e21642bb95d2cee
Patricia Disaign <patriciandrea.p@gmail.com>
2016-10-17 23:22:17 -0300

    ejercicios terminados

--- js/script.js
+++ js/script.js
@@ -100,6 +100,78 @@ function calcularHoras (arrayHoras, valorHora){
 	console.log("El sueldo es " + total);
 }
 
+// EJERCICIO 7
+// calcular promedio de estatura según cantidad de personas
 
+function promedio (ArrayEstatura){
+	var promedioEstatura = 0;
+
+	for( var i = 0; i < ArrayEstatura.length; i++){
+			promedioEstatura = promedioEstatura + ArrayEstatura[i];
+
+		}
+		return promedioEstatura/ArrayEstatura.length;
+		console.log(promedio);
+}
+
+// EJERCICIO 8
+// Ordenar una serie de numeros de menor a mayores
+// function comparacion (a, b){
+// 	return a - b  
+// }
+
+function orden (serieNumeros){
+
+	var ordenados = serieNumeros.sort((a,b)=>a-b);
+
+	return ordenados;
+}
+
+// EJERCICIO 9 
+// Se requieres saber cuantas ventas:
+// > a 1000
+// > a 500 pero < o = a 1000
+// < = a 500
+
+// se guardará en casilla [0]] la cantidad de ventas, y en [1] el valor de cada ventas
+// // después se llama a las posiciones
+
+function tikiTaka(arregloVentas){
+  var menos500 = [0,0];
+  var entre500y1000 = [0,0];
+  var mas1000 = [0,0];
+
+  for(var i = 0 ; i < arregloVentas.length ; i++){
+    if(arregloVentas[i]<500){
+      menos500[0]=menos500[0]+1;
+      menos500[1]=menos500[1]+arregloVentas[i];
+    }else if(arregloVentas[i]>1000){
+      mas1000[0]=mas1000[0]+1;
+      mas1000[1]=mas1000[1]+arregloVentas[i];
+    }else{
+      entre500y1000[0]=entre500y1000[0]+1;
+      entre500y1000[1]=entre500y1000[1]+arregloVentas[i];
+    }
+  }
+  console.log(menos500[0]+" Ventas menores de $500 por: $"+menos500[1]);
+  console.log(entre500y1000[0]+" Ventas entre los $500 y $1000 por: $"+entre500y1000[1]);
+  console.log(mas1000[0]+" Ventas sobre $1000 por: $"+mas1000[1]);
+}
+
+// EJERCICIO 10
+// Programa que sume el cuadrado de dos numeros
+
+function sumaCuadrados (){
+	var numeroUno = prompt("Ingresa un numero entero");
+	var numeroDos = prompt("Ingresa otro numero entero");
+	var resultadouno = (parseInt(numeroUno) + parseInt(numeroDos));
+	var resultado = (parseInt(resultadouno) * parseInt(resultadouno));
+	
+	alert("El cuadrado de la suma de los números que ingresaste es " + resultado);
+
+}
+
+
+// Lo logré :)
 
 
